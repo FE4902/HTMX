674237567be29de53d0ac362edd8669bb760da05
FE4902 <isdn000123@gmail.com>
2024-04-09 21:37:10 +0900

    Protected Routes Protected Routes Protected Routes

--- src/routes/home.tsx
+++ src/routes/home.tsx
@@ -1,3 +1,12 @@
-export default function home() {
-    return <h1>home</h1>;
+import { auth } from "../firebase";
+
+export default function Home() {
+    const logOut = () => {
+        auth.signOut();
+    };
+    return (
+        <h1>
+            <button onClick={logOut}>Log Out</button>
+        </h1>
+    );
 }
